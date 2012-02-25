7beb327a503ec915ea4ac19bd6ce8c3fca1169dc
Amit Erandole <amit.erandole@gmail.com>
2012-02-25 20:13:09 +0530

    removed hard coded min max values
    These are being passed from the outside now
    
    Signed-off-by: Amit Erandole <amit.erandole@gmail.com>

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,6 @@
 /* 
-wrap the app in a self-invoking anonymous function to prevent leakage of global variables
+wrap the app in a self-invoking anonymous function to prevent messing up scope
+with global variables
 */
 (function ($) {
 	
@@ -17,26 +18,35 @@ wrap the app in a self-invoking anonymous function to prevent leakage of global 
 		cache: function (config) {
 			this.sl = config.slider;
 			this.output = config.output;
+			this.minVal = config.min;
+			this.maxVal = config.max;
+			this.anim = config.animate;
+
 			// this.logger(config.output);
 		},
 
 		createSlider: function () {
+			// hold on to the value of this
 			var self = CloudCalc;			
+			
+			// initialize slider with basic properties and defaults if no values are passed in
 			self.sl.slider({
-				animate: true,
-				min: 1,
-				max: 8
+				animate: self.anim || false, // TODO: may be redundant to set this to false
+				min: self.minVal || 1,
+				max: self.maxVal || 8
 			});
 
 			return self;			
 		},
 
 		setSliderOutputValue: function () {
+			// hold on to the value of this
 			var self = CloudCalc;
 
+			// show the ui output value on slide event
 			self.sl.slider({
 				slide: function ( event, ui ) {
-					 self.output.html( ui.value + " INSTANCE");	
+					self.output.html( ui.value + " INSTANCE");	
 				}
 			});
 
@@ -52,6 +62,9 @@ wrap the app in a self-invoking anonymous function to prevent leakage of global 
 	// kick things off
 	window.CloudCalc = CloudCalc.init({
 		slider: $('#slider'),
-		output: $('#slider-result') 
+		output: $('#slider-result'),
+		min: 1,
+		max: 8,
+		animate: true 
 	});
 })(jQuery);
