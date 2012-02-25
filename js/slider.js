2f8caaed0d4924ac77640ff0cfa4860e1f4a8ef1
Amit Erandole <amit.erandole@gmail.com>
2012-02-25 18:07:26 +0530

    started from scratch with a modular approach
    Signed-off-by: Amit Erandole <amit.erandole@gmail.com>

--- js/slider.js
+++ js/slider.js
@@ -1,32 +1,30 @@
-$( ".slider" ).slider({
-	animate: true,
-	range: "min",
-	value: 50,
-	min: 10,
-	max: 100,
-	step: 10,
-//this gets a live reading of the value and prints it on the page
-slide: function( event, ui ) {
-    $( "#slider-result" ).html( ui.value + " GB");
-},
+/* 
+wrap the app in a self-invoking anonymous function to prevent leakage of global variables
+*/
+(function ($) {
+	
+	var CloudCalc = {
+		
+		// bootstrap the app
+		init: function (config) {
+			console.log('app init');
+			this.cache(config);
+		},
 
-//this updates the hidden form field so we can submit the data using a form
-change: function(event, ui) { 
-$('#hidden').attr('value', ui.value);
-		var val = ui.value,
-		$box = $('#message');
-		switch(true)
-		{
-		case (val > 50):
-		  $box.fadeOut().fadeIn().html('you have chosen xtremenet');
-		  break;
-		case (val < 50):
-		  $box.fadeOut().fadeIn().html('you have chosen valuenet');
-		  break;
-		default:
-		  $box.fadeOut().fadeIn().html('you have chosen powernet');
+		cache: function (config) {
+			var slider = config.slider;
+			// this.callme(slider);
+		},
+
+		callme: function (msg) {
+			console.log(msg);	
 		}
-	}
-});
 
-$("#message").fadeIn(800);
+	}; //end of CloudCalc
+
+	// kick things off
+	window.CloudCalc = CloudCalc.init({
+		slider: $('#slider') 
+	});
+
+})(jQuery);
