a5d117c8dd49dc07351a8e8d0e8f94f79a4267a7
Amit Erandole <amit.erandole@gmail.com>
2012-02-25 17:46:29 +0530

    indentation changes
    Signed-off-by: Amit Erandole <amit.erandole@gmail.com>

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,10 @@
 $( ".slider" ).slider({
-			animate: true,
-range: "min",
-value: 50,
-min: 10,
-max: 100,
-				step: 10,
-
+	animate: true,
+	range: "min",
+	value: 50,
+	min: 10,
+	max: 100,
+	step: 10,
 //this gets a live reading of the value and prints it on the page
 slide: function( event, ui ) {
     $( "#slider-result" ).html( ui.value + " GB");
@@ -14,21 +13,20 @@ slide: function( event, ui ) {
 //this updates the hidden form field so we can submit the data using a form
 change: function(event, ui) { 
 $('#hidden').attr('value', ui.value);
-				var val = ui.value,
-				$box = $('#message');
-				switch(true)
-				{
-				case (val > 50):
-				  $box.fadeOut().fadeIn().html('you have chosen xtremenet');
-				  break;
-				case (val < 50):
-				  $box.fadeOut().fadeIn().html('you have chosen valuenet');
-				  break;
-				default:
-				  $box.fadeOut().fadeIn().html('you have chosen powernet');
-				}
-			}						
-
+		var val = ui.value,
+		$box = $('#message');
+		switch(true)
+		{
+		case (val > 50):
+		  $box.fadeOut().fadeIn().html('you have chosen xtremenet');
+		  break;
+		case (val < 50):
+		  $box.fadeOut().fadeIn().html('you have chosen valuenet');
+		  break;
+		default:
+		  $box.fadeOut().fadeIn().html('you have chosen powernet');
+		}
+	}
 });
 
 $("#message").fadeIn(800);
