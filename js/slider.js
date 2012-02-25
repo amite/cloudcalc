0cd7299161142521cff5432ad0893e21dc6cf76a
Amit Erandole <amit.erandole@gmail.com>
2012-02-25 20:00:24 +0530

    output is working
    Signed-off-by: Amit Erandole <amit.erandole@gmail.com>

--- js/slider.js
+++ js/slider.js
@@ -9,25 +9,38 @@ wrap the app in a self-invoking anonymous function to prevent leakage of global 
 		init: function (config) {
 			this.cache(config);
 			this.createSlider();
-
+			this.setSliderOutputValue();
 			return this;
 		},
 
+		// cache all variables related to this app
 		cache: function (config) {
-			this.slider = config.slider;
-			this.logger(slider);
+			this.sl = config.slider;
+			this.output = config.output;
+			// this.logger(config.output);
 		},
 
 		createSlider: function () {
+			var self = CloudCalc;			
+			self.sl.slider({
+				animate: true,
+				min: 1,
+				max: 8
+			});
+
+			return self;			
+		},
+
+		setSliderOutputValue: function () {
 			var self = CloudCalc;
-			
-			self.slider.slider({
-				range: "min",
-			 	value: 50,
-			 	min: 10,
-			 	max: 100,
-			 	step: 10
-			 })	
+
+			self.sl.slider({
+				slide: function ( event, ui ) {
+					 self.output.html( ui.value + " INSTANCE");	
+				}
+			});
+
+			return self;	
 		},
 
 		logger: function (msg) {
@@ -38,6 +51,7 @@ wrap the app in a self-invoking anonymous function to prevent leakage of global 
 
 	// kick things off
 	window.CloudCalc = CloudCalc.init({
-		slider: $('#slider') 
+		slider: $('#slider'),
+		output: $('#slider-result') 
 	});
 })(jQuery);
